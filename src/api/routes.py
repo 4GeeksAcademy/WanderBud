be4b0b122caa569c48a5b5fc6ce6cd5b7f6eb4a2
Osián <149536063+OsianJL@users.noreply.github.com>
2024-04-11 19:41:47 +0000

    login backend second commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -31,15 +31,11 @@ def handle_hello():
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    print(email)
-    print(password)
 
     query_results = User.query.filter_by(email=email).first()
 
     if query_results is None:
             return jsonify({"msg": "Bad Request"}), 404
-    print(query_results.email)
-    print(query_results.password)
 
     if email == query_results.email and password == query_results.password:
             access_token = create_access_token(identity=email)
@@ -56,7 +52,7 @@ def valid_token():
      current_user = get_jwt_identity()
 
      querty_results = User.query.filter_by(email=current_user).first()
-     
+
      if querty_results is None:
             return jsonify({"msg": "user does not exist",
                            "is_logged": False}), 404
