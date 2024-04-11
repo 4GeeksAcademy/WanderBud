e4a01b0630310fbacdc6ff5a3e8d3993ee3d6dec
Osián <149536063+OsianJL@users.noreply.github.com>
2024-04-11 19:35:45 +0000

    login backend first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,41 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    print(email)
+    print(password)
+
+    query_results = User.query.filter_by(email=email).first()
+
+    if query_results is None:
+            return jsonify({"msg": "Bad Request"}), 404
+    print(query_results.email)
+    print(query_results.password)
+
+    if email == query_results.email and password == query_results.password:
+            access_token = create_access_token(identity=email)
+            return jsonify(access_token=access_token), 200
+    
+    else: 
+            return jsonify({"msg": "Bad email or password. I am sorry"}), 401
+
+
+@api.route("/valid-token", methods=["GET"])
+@jwt_required()
+def valid_token():
+     
+     current_user = get_jwt_identity()
+
+     querty_results = User.query.filter_by(email=current_user).first()
+     
+     if querty_results is None:
+            return jsonify({"msg": "user does not exist",
+                           "is_logged": False}), 404
+     
+     return jsonify({"is_logged": True}), 200
