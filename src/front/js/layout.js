db14bb414924dcd5d63c63a6f2bd0fa38c918219
Osián <149536063+OsianJL@users.noreply.github.com>
2024-04-12 14:01:08 +0000

    styles ok

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -24,7 +24,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    {/* <Navbar /> */}
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<PasswordRecoveryForm />} path="/password-recovery" />
@@ -32,7 +32,7 @@ const Layout = () => {
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
+                    {/* <Footer /> */}
                 </ScrollToTop>
             </BrowserRouter>
         </div>
