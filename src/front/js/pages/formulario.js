66126ed9f5d748e176048be20978a79c56f0bc69
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-04-15 17:53:52 +0000

    commit

---
+++ src/front/js/pages/formulario.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+	export const Formulario = () => {
+		const { store, actions } = useContext(Context);
+		return (
+			<div><h1>QUE PASA</h1></div>
+
+    );
+};
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,46 +1,44 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom"; 
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 	export const Home = () => {
 		const { store, actions } = useContext(Context);
 		return (
-			<div className="container" style={{ backgroundColor: '#1a1f25', color: '#fff', minHeight: '100vh', paddingTop: '50px', position: 'relative' }}>
-    <div className="row justify-content-center">
-        {/* Columna para el texto "WanderBud" */}
-        <div className="col-md-6">
-            <div style={{ position: 'relative', textAlign: 'right' }}>
-                {/* Texto "WanderBud" centrado y desplazado hacia la derecha */}
-                <h1 style={{ fontSize: '3rem', fontWeight: 'bold', color: '#fff' }}>WanderBud</h1>
+			<div className="container-fluid" style={{ backgroundColor: '#1a1f25', color: '#fff', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div className="row justify-content-center">
+                {/* Columna para el texto "WanderBud" */}
+                <div className="col-md-6 text-right">
+                    <div>
+                        {/* Texto "WanderBud" centrado y desplazado hacia la derecha */}
+                        <h1 style={{ fontSize: '3rem', fontWeight: 'bold', color: '#fff', marginBottom: '20px' }}>WanderBud</h1>
 
-                {/* Imagen de fondo debajo de "WanderBud" (más pequeña) */}
-                <img src="https://res.cloudinary.com/dkfphx3dm/image/upload/v1713005832/Logo2_t2gs97.jpg" className="img-fluid" alt="Imagen de fondo" style={{ position: 'absolute', top: '375%', left: '75%', transform: 'translate(-50%, -50%)', maxWidth: '330px', maxHeight: '330px', objectFit: 'cover' }} />
-            </div>
-        </div>
-
-        {/* Columna para el formulario de inicio de sesión */}
-        <div className="col" style={{ width: '300px' }}>
-            <div className="card p-4 mt-5" style={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)', width: '60%'}}>
-                <h4 className="text-center mb-4">Connect. Create. Celebrate.</h4>
-                <form>
-                    <button type="submit" className="btn btn-primary btn-block rounded-pill mb-3" style={{ width: '60%' }}>Login</button>
-                </form>
-                {/* Establece la anchura del botón de Google según la anchura del botón de Login */}
-                <button type="submit" className="btn btn-danger rounded-pill mb-3" style={{ width: '60%' }}>Google</button>
-                
-                {/* Línea horizontal con texto y líneas decorativas */}
-                <div className="text-center" style={{ margin: '20px 0', position: 'relative', maxWidth: '200px', width: '80%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <div className="line" style={{ flex: '1', height: '1px', backgroundColor: '#000000' }}></div>
-                    <div style={{ padding: '0 10px', fontSize: '12px' }}>If you don't have one</div>
-                    <div className="line" style={{ flex: '1', height: '1px', backgroundColor: '#000000' }}></div>
+                        {/* Imagen de fondo debajo de "WanderBud" (más pequeña) */}
+                        <img src="https://res.cloudinary.com/dkfphx3dm/image/upload/v1713005832/Logo2_t2gs97.jpg" className="img-fluid" alt="Imagen de fondo" style={{ maxWidth: '330px', maxHeight: '330px', objectFit: 'cover' }} />
+                    </div>
                 </div>
 
-                <button type="submit" className="btn btn-light btn-block rounded-pill mb-3 border" style={{ width: '60%' }}>Sign Up</button>
+                {/* Columna para el formulario de inicio de sesión */}
+                <div className="col-md-4">
+                    <div className="card p-4" style={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)' }}>
+                        <h4 className="text-center mb-4">Connect. Create. Celebrate.</h4>
+                        <form>
+                            <button type="submit" className="btn btn-primary btn-block rounded-pill mb-3">Login</button>
+                        </form>
+                        {/* Botón de inicio de sesión con Google */}
+                        <form>
+                            <button type="submit" className="btn btn-danger btn-block rounded-pill mb-3">Google</button>
+                        </form>
+                        {/* Línea horizontal con texto */}
+                        <div className="text-center mb-3" style={{ borderBottom: '1px solid #000', lineHeight: '0.1em' }}>
+                            <span style={{ background: '#fff', padding: '0 10px', fontSize: '12px' }}>If you don't have one</span>
+                        </div>
+                        {/* Botón de registro */}
+                        <button type="submit" className="btn btn-light btn-block rounded-pill border" style={{ textDecoration: 'none' }}>Sign Up</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
     );
 };
