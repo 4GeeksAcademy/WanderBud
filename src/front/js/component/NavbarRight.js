de51ce6517dcf01ca0768754ea0ea643754d6a56
frankluis1923 <148373205+frankluis1923@users.noreply.github.com>
2024-04-19 09:59:45 +0000

    nabvarRight superior listo

--- src/front/js/component/NavbarRight.js
+++ src/front/js/component/NavbarRight.js
@@ -8,16 +8,16 @@ export const NavbarRight = () => {
   };
 
   return (
-    <div style={{ position: "absolute", top: 0, right: 0, left: 0, padding: "0px", backgroundColor: "black", color: "white", display: "flex", alignItems: "center" }}>
-      <span style={{ fontSize: "1rem", fontWeight: "normal", marginLeft: "30px" }}>Create New Event</span>
-      <button style={{ backgroundColor: "#0C0C0C", 
+    <div style={{ position: "absolute", top: 0, right: 0, left: 0, padding: "0px", color: "white", display: "flex", alignItems: "center" }}>
+      <span style={{ fontSize: "1rem", fontWeight: "helvetica", marginLeft: "15px",fontSize:"20px" }}>Create New Event</span>
+      <button style={{ backgroundColor: "#1a1f25", 
                       color: "white", 
-                      fontWeight: "normal", 
+                      fontWeight: "helvetica", 
                       borderRadius: "20px", 
-                      padding: "6px 18px", 
+                      padding: "4px 10px", 
                       marginLeft: "40px", 
-                      border: "none", width: "auto", 
-                      marginTop: "20px", }} onClick={handleClick}>
+                      border: "1px solid white", width: "auto", 
+                      marginTop: "12px",fontSize:"20px" }} onClick={handleClick}>
         Create
       </button>
     </div>
