071ca9b46127be7c6e0e9c77ae619e7289211959
Osián <149536063+OsianJL@users.noreply.github.com>
2024-04-19 19:38:04 +0000

    ready to merge

--- src/front/js/component/eventPublicView.js
+++ src/front/js/component/eventPublicView.js
@@ -19,17 +19,18 @@ export const EventPublicView = () => {
 
     return (
         <>
-            <div className="container-event d-block">
+            <div className="row flex-column">
                 {store.publicEvents.map((item, index) => {
                     console.log(item); // Aquí agregamos el console.log para ver el contenido de cada item
                     return (
-                        <div className="card-event mb-3" key={index}>
+                        <div className="card-event col-12 mb-3" key={index}>
                             <div className="card2-event">
                                 <div className="card-body-event">
-                                    <div className="card-top">
+                                    <div className="card-top d-flex justify-content-between">
+                                            
+                                        <h3 className="card-title-event col-8">{item.name}</h3>
 
-                                        <h3 className="card-title-event">{item.name}</h3>
-                                        <button className="boton-join">Join Us!</button>
+                                        <button className="boton-join col-2">Join Us!</button>
                                     </div>
                                     <h4 className="card-date-event mt-3">{item.start_date},   {item.start_time}-{item.end_time}</h4>
                                     <h4 className="card-location-event mt-3">{item.location}</h4>
