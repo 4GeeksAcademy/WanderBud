04fd41607fd843140b2efb6a1bd5d70a5da690fb
Bruno Murua <100427363+SrZeroRequiem@users.noreply.github.com>
2024-04-22 01:08:12 +0000

    Left Navbar and Main Feed Tabs Finished, Rightnav In Progress

--- src/front/js/component/NavbarRight.js
+++ src/front/js/component/NavbarRight.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Col, Row, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FaPlusCircle } from 'react-icons/fa'
 
@@ -9,16 +9,42 @@ const NavbarRight = () => {
   const handleClick = () => {
     navigate("/create-event");
   };
+  const RequestsCard = ({ username, eventname, img }) => (
+    <Card.Body className="m-0 py-2 px-1 row w-100 request-container">
+      <Col md={2} className="p-1">
+        <img src={img} alt={username + "'s Photo"} className="rounded img-fluid" />
+      </Col>
+      <Col md={7} className="d-flex flex-column align-items-start object-fit-cover">
+        <p className="m-0">{username}</p>
+        <span className="m-0 text-muted">{eventname}</span>
+      </Col>
+      <Col md={3} className="d-flex align-items-center justify-content-end">
+        <Button variant="success" className="rounded-circle me-1">A</Button>
+        <Button variant="danger" className="rounded-circle">R</Button>
+      </Col>
+    </Card.Body>
+  );
 
   return (
-    <Container fluid className="h-100 d-flex align-items-start justify-content-center sidenav sidenav-right p-4">
-      <Button
-        variant="create"
-        className="d-flex align-items-center justify-content-center w-75"
-        onClick={handleClick}
-      >Create New Event
-        <FaPlusCircle className="ms-2" />
-      </Button>
+    <Container fluid className="h-100 d-flex align-items-start justify-content-center sidenav sidenav-right pt-4 px-4 m-0">
+      <Row className="w-100">
+        <Col md={12}>
+          <Button
+            variant="create"
+            className="d-flex align-items-center justify-content-between w-100 rounded-pill m-0"
+            onClick={handleClick}
+          >Create New Event
+            <FaPlusCircle className="ms-2" />
+          </Button>
+        </Col>
+        <Col md={12} className="mt-4 pe-0">
+          <Card className="request-container">
+            <Card.Header>Join Requests</Card.Header>
+            <RequestsCard username={"Bruno Murua"} eventname={"Dune 2 at the Cinema"} img={"https://media.licdn.com/dms/image/D4D03AQHepyIMxVGZ6A/profile-displayphoto-shrink_800_800/0/1703165598705?e=1719446400&v=beta&t=G3Qe-5D8glPTTi5Ovn20LKlI4no3y6qNuPucDm6ZaNU"} />
+            <RequestsCard username={"Osian Lezcano"} eventname={"Alone in my House :("} img={"https://ca.slack-edge.com/T0BFXMWMV-U064ZSVS678-877ce3b7e9a7-512"} />
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/front/js/component/leftsidebar.js
+++ src/front/js/component/leftsidebar.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Nav, Navbar, Button } from 'react-bootstrap';
-import { FaHome, FaUser, FaCog } from 'react-icons/fa'; // Importa los iconos de FontAwesome
-import { Link } from 'react-router-dom';
+import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 
 const LeftSidebar = ({ changeContent }) => {
+    const [path, setPath] = useState(null);
+    const [settingsOptions, setSettingsOptions] = useState("w-100 d-flex align-content-end flex-column d-none");
+    const [settings, setSettings] = useState("btn setting-container w-75 p-0");
+    const [settingsBtn, setSettingsBtn] = useState("");
+
+    const location = useLocation();
     const handleItemClick = (option) => {
         changeContent(option);
     };
+    useEffect(() => {
+        setPath((location.pathname).toLocaleLowerCase());
+    }
+        , [location]);
+    const handlePath = (option) => {
+        if (path === option) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+    const handleSettings = () => {
+        if (settingsOptions.includes('d-none')) {
+            setSettingsOptions('w-100 d-flex align-content-end flex-column active');
+            setSettings('btn setting-container w-75 p-0 active');
+            setSettingsBtn('active mb-2');
+        } else {
+            setSettingsOptions('w-100 d-flex align-content-end flex-column d-none');
+            setSettings('btn setting-container w-75 p-0');
+            setSettingsBtn('');
+        }
+    }
+
+
+    const SidebarButton = ({ to, icon, text, w, variant }) => (
+        <Nav.Item className={`w-${w} mb-2`}>
+            <Link to={to}>
+                <Button variant={variant} className={handlePath(to) + " justify-content-start"}>
+                    {icon}
+                    {text}
+                </Button>
+            </Link>
+        </Nav.Item>
+    );
+
+    const SettingsButton = () => (
+        <Nav.Item className={settings}>
+            <Link to='#'>
+                <Button variant="sidenav" className={settingsBtn} onClick={handleSettings}>
+                    <FaCog className="me-2" /> Settings
+                </Button>
+            </Link>
+            <Nav.Item className={settingsOptions + " setting-option"}>
+                <SidebarButton to='/settings/account' text='Account' w="100" variant={'navtab'} />
+                <SidebarButton to='/settings/notifications' text='Notifications' w="100" variant={'navtab'} />
+                <SidebarButton to='/settings/privacy' text='Privacy' w="100" variant={'navtab'} />
+                <SidebarButton to='/settings/security' text='Security' w="100" variant={'navtab'} />
+                <Button variant="logout" className='w-100'>
+                    Log out
+                </Button>
+            </Nav.Item>
+        </Nav.Item>
+    );
 
     return (
         <Navbar expand="lg" className="flex-column justify-content-between vh-100 sidenav sidenav-left p-0">
@@ -14,44 +73,9 @@ const LeftSidebar = ({ changeContent }) => {
                 <h1>WanderBud</h1>
             </Navbar.Brand>
             <Nav className="p-0 d-flex flex-column w-100 justify-content-start align-items-center h-100">
-                <Nav.Item className='w-75 mb-2'>
-                    <Link to='/feed'>
-                        <Button variant="sidenav" className=''>
-                            <FaHome className="me-2" /> Feed
-                        </Button>
-                    </Link>
-                </Nav.Item>
-                <Nav.Item className='w-75 mb-2'>
-                    <Link to='/profile'>
-                        <Button variant="sidenav">
-                            <FaUser className="me-2" /> Profile
-                        </Button>
-                    </Link>
-                </Nav.Item>
-                <Nav.Item className='btn setting-container active w-75 mb-2 p-0 '>
-                    <Link to='/settings'>
-                        <Button variant="sidenav" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" className='active mb-2'>
-                            <FaCog className="me-2" /> Settings
-                        </Button>
-                    </Link>
-                    <Nav.Item className='w-100 d-flex align-content-end flex-column'>
-                        <Button variant="sidenav" className='w-100 mb-2'>
-                            Account
-                        </Button>
-                        <Button variant="sidenav" className='w-100 mb-2'>
-                            Notifications
-                        </Button>
-                        <Button variant="sidenav" className='w-100 mb-2'>
-                            Privacy
-                        </Button>
-                        <Button variant="sidenav" className='w-100 mb-2'>
-                            Security
-                        </Button>
-                        <Button variant="logout" className='w-100'>
-                            Log out
-                        </Button>
-                    </Nav.Item>
-                </Nav.Item>
+                <SidebarButton to='/feed' icon={<FaHome className='me-2' />} text='Feed' w="75" variant={'sidenav'} />
+                <SidebarButton to='/profile' icon={<FaUser className='me-2' />} text='Profile' w="75" variant={'sidenav'} />
+                <SettingsButton />
             </Nav>
         </Navbar>
     );
