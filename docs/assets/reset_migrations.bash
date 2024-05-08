fe8383bb82a7c3ebdc4ddfd61ee907f959ac2d7f
Bruno Murua <100427363+SrZeroRequiem@users.noreply.github.com>
2024-05-08 16:10:02 +0000

    reset_command

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -1,8 +1,22 @@
+#!/bin/bash
+
+# Definir la cadena de conexión
+DB_CONNECTION="postgres://postgresql_trapezoidal_42170_fhsx_user:TcId8gAfo75qxZcfdnb6gA7ejgYXWrRZ@dpg-cot98bfsc6pc73cfrbpg-a/postgresql_trapezoidal_42170_fhsx"
+
+# Ejecutar las acciones necesarias
+echo "Reinicio de migraciones"
 rm -R -f ./migrations &&
+echo "Eliminación de la base de datos existente" &&
 pipenv run init &&
-dropdb -h localhost -U gitpod example || true &&
-createdb -h localhost -U gitpod example || true &&
-psql -h localhost example -U gitpod -c 'CREATE EXTENSION unaccent;' || true &&
+echo "Eliminación de la base de datos existente" &&
+dropdb "$DB_CONNECTION" || true &&
+echo "Creación de la base de datos" &&
+createdb "$DB_CONNECTION" || true &&
+echo "Creación de la extensión 'unaccent'" &&
+psql "$DB_CONNECTION" -c 'CREATE EXTENSION IF NOT EXISTS unaccent;' || true &&
+echo "Migración" &&
 pipenv run migrate &&
+echo "Actualización" &&
 pipenv run upgrade &&
+echo "Inserción de datos de prueba" &&
 pipenv run insert-test-data
--- migrations/versions/34ba928eebf2_.py
+++ migrations/versions/34ba928eebf2_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 129aef25cc4c
+Revision ID: 34ba928eebf2
 Revises: 
-Create Date: 2024-05-07 20:45:52.306339
+Create Date: 2024-05-07 20:54:36.999572
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '129aef25cc4c'
+revision = '34ba928eebf2'
 down_revision = None
 branch_labels = None
 depends_on = None
